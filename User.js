f742e0764f71bc7225f7e7ba6e604d4186cee567
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-08-21 19:46:02 +0530

    Added Schema Validation

--- User.js
+++ User.js
@@ -8,10 +8,30 @@ const addressSchema = new mongoose.Schema({
 
 const userSchema = new mongoose.Schema({
     name: String,
-    age:Number,
-    email: String,
-    createdAt: Date,
-    updatedAt: Date,
+    age:{
+        type: Number,
+        min: 1,
+        max: 100,
+        validate:{
+            validator: v => v % 2 === 0,
+            message: props => `${props.value} is not an even number`,
+        }
+    },
+    email: {
+        type: String,
+        minlength: 10,
+        required: true,
+        lowercase: true,
+    },
+    createdAt: {
+        type: Date,
+        immutable: true,
+        default: () => Date.now(),
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now(),
+    },
     bestFriend: mongoose.SchemaTypes.ObjectId,
     hobbies: [String],
     address: addressSchema,
--- script.js
+++ script.js
@@ -5,17 +5,27 @@ mongoose.connect("mongodb+srv://prajudharpure:prajakta@cluster0.vzgzz.mongodb.ne
 
 run()
 async function run() {
+    try{
     const user = await User.create({
         name: "praju", 
         age: 22,
+        email: "praju@gmail.com",
         hobbies: ["calligraphy", "basket ball"],
         address: {
             street: "Main street",
         },
     })
+    user.createdAt = 5
+    await user.save()
+    console.log(user)
+} catch (e) {
+    // console.log(e.message)
+    console.log(e.message)
+}
+}
     // user.name = "prajuu"
     // await user.save()
 // const user = new User({ name: "Praju", age: 26 })
 // await user.save()
-console.log(user)
-}
+// console.log(user)
+// }
