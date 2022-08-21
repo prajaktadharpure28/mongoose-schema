fd11e07c4d3ac944f0fec324c2519dcaf18a4ba1
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-08-21 19:11:33 +0530

    Schema type

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose')
 const User = require("./User")
 
-mongoose.connect("mongodb://localhost/testdb")
+mongoose.connect("mongodb+srv://prajudharpure:prajakta@cluster0.vzgzz.mongodb.net/whatsapp?retryWrites=true&w=majority")
 
 run()
 async function run() {
-    const user = await User.create({ name: "praju", age: 22})
-    user.name = "prajuu"
-    await user.save()
+    const user = await User.create({
+        name: "praju", 
+        age: 22,
+        hobbies: ["calligraphy", "basket ball"],
+        address: {
+            street: "Main street",
+        },
+    })
+    // user.name = "prajuu"
+    // await user.save()
 // const user = new User({ name: "Praju", age: 26 })
 // await user.save()
 console.log(user)
